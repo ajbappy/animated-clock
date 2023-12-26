6f3cd2bb95b6c6f13646c0841ac8eaeac3c92cb0
ajbappy <ajbappywahid22@gmail.com>
2023-12-26 20:46:16 +0600

    clock all functionality created

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const clock = () => {
   // set up style
 
   ctx.strokeStyle = "#000";
-  ctx.fillStyle = " #3296FF";
+  ctx.fillStyle = " #E4E7E0";
   ctx.lineWidth = 5;
   ctx.clneCap = "round";
 
@@ -22,7 +22,7 @@ const clock = () => {
   ctx.save();
   ctx.beginPath();
   ctx.lineWidth = 10;
-  ctx.strokeStyle = "#141319";
+  ctx.strokeStyle = "#000";
   ctx.arc(0, 0, 140, 0, Math.PI * 2, true);
   ctx.stroke();
   ctx.fill();
@@ -46,16 +46,72 @@ const clock = () => {
   ctx.save();
   ctx.lineWidth = 2;
   for (let i = 0; i < 60; i++) {
-    ctx.beginPath();
+    if (i % 5 != 0) {
+      ctx.beginPath();
+
+      ctx.moveTo(115, 0);
+      ctx.lineTo(120, 0);
+      ctx.stroke();
+    }
     ctx.rotate(Math.PI / 30);
-    ctx.moveTo(115, 0);
-    ctx.lineTo(120, 0);
-    ctx.stroke();
   }
 
   ctx.restore();
 
+  //   get corrent time
+
+  const hr = now.getHours() % 12;
+  const mins = now.getMinutes();
+  const sec = now.getSeconds();
+
+  //   Draw hours hand
+
+  ctx.save();
+  ctx.lineWidth = 14;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#800000";
+  ctx.rotate(
+    (Math.PI / 6) * hr + (Math.PI / 360) * mins + (Math.PI / 21600) * sec
+  );
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  //   Draw mins hand
+
+  ctx.save();
+  ctx.lineWidth = 10;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#800000";
+  ctx.rotate((Math.PI / 30) * mins + (Math.PI / 1800) * sec);
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(110, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  //   Draw sec hand
+
+  ctx.save();
+  ctx.lineWidth = 5;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#000";
+  ctx.fillStyle = "#000";
+  ctx.rotate((sec * Math.PI) / 30);
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(110, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 12, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.restore();
+
   ctx.restore(); // restore default satate
+
+  requestAnimationFrame(clock);
 };
 
-clock();
+requestAnimationFrame(clock);
